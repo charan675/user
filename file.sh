1e2880adc106a864891d2ce3556c9d74f33e125a
charan675 <65114117+charan675@users.noreply.github.com>
2023-05-30 22:47:56 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
 sudo apt update
-sudo apt full-upgrade
+sudo apt full-upgrade -y
 sudo git clone https://github.com/venkataprasanna45/hearing.git
-cd /
-mv hearing /home/ubuntu
-cd /home/ubuntu
 cd hearing
 sudo apt install python3-pip -y
-pip3 install -r requirements.txt
-screen -m -d python3 app.py
+sudo pip3 install -r requirements.txt
+sudo python3 app.py
